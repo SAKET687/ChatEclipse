4f4383ae4f08f8e51559082e13075b311d10bec0
SAKET SRIVASTAVA <127039760+SAKET687@users.noreply.github.com>
2024-01-04 03:02:22 +0530

    12th commit

--- public/script.js
+++ public/script.js
@@ -4,6 +4,84 @@ function enter() {
     localStorage.setItem("userName", userName);
     window.location = "newindex.html";
   } else {
-    alert("Please enter your name to proceed.😀");
+    alert(
+      "Please enter your name to proceed 😃 or choose the 'Continue as Guest' option 🌟."
+    );
   }
+}
+
+function enterAsGuest() {
+  const randomNames = [
+    "Luna 👩‍🚀",
+    "Neo 😎",
+    "Aurora 🌌",
+    "Phoenix 🔥",
+    "Zephyr 😀",
+    "Nova 💫",
+    "Jupiter 🪐",
+    "Orion 🌌",
+    "Seraphina 👼",
+    "Atlas 🗺️",
+    "Lyra 🎵",
+    "Cassius ⚔️",
+    "Gaia 🌍",
+    "Selene 🌕",
+    "Athena 🦉",
+    "Griffin 🦅",
+    "Serenity ☮️",
+    "Kairos ⏳",
+    "Ember 🔥",
+    "Maven 📚",
+    "Ragnar ⚒️",
+    "Nyx 🌑",
+    "Astrid ✨",
+    "Zara 👑",
+    "Calypso 🌊",
+    "Ezra 📜",
+    "Thalia 🌼",
+    "Kai 🌊",
+    "Sylvan 🌲",
+    "Isolde 🏰",
+    "Xander ⚔️",
+    "Freya ⚡",
+    "Nova 🌟",
+    "Solstice 🌞",
+    "Eclipse 🌒",
+    "Raven 🦅",
+    "Crimson 🔴",
+    "Zephyra 🌀",
+    "Stella ✨",
+    "Aurora 🌅",
+    "Blaze 🔥",
+    "Titan 🏋️‍♂️",
+    "Iris 🌈",
+    "Aria 🎶",
+    "Olympia 🏛️",
+    "Cypher 🤖",
+    "Sable 🌑",
+    "Nova 🌌",
+    "Electra ⚡",
+    "Spectra 🌈",
+    "Lumina ✨",
+    "Cosmo 🚀",
+    "Rogue 🗡️",
+    "Zenith 🌠",
+    "Echo 🗣️",
+    "Elysium 🌅",
+    "Mystique 🔮",
+    "Reverie 💭",
+    "Haven 🏝️",
+    "Sapphire 💎",
+    "Chaos 🌀",
+    "Tempest 🌪️",
+    "Oracle 🔮",
+    "Mercury ☿️",
+    "Cipher 🤫",
+    "Aether ☁️",
+  ];
+
+  var enteredName = randomNames[Math.floor(Math.random() * randomNames.length)];
+  alert(`We've assigned you the name '${enteredName}' for the chat! 🌟`);
+  localStorage.setItem("userName", enteredName);
+  window.location = "newindex.html";
 }
